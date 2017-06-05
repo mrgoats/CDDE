1f1168dbf254bd157f1e948490662533083eb13d
Douglas Ferreira <drdocf@outlook.com>
2017-06-05 01:04:02 -0300

    Van Gogh - Configuração do ambiente de desenvolvimento

--- karma.conf.js
+++ karma.conf.js
@@ -2,12 +2,18 @@ module.exports = function (config) {
     config.set({
 
         basePath: '',
+        logLevel: config.LOG_DEBUG,
+        singleRun: true,
         frameworks: ['jasmine'],
         files: [
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/fabric/dist/fabric.min.js',
+            'bower_components/threejs/build/three.min.js',
 
             'app/*.module.js',
+            'app/*.factory.js',
+            'app/*.service.js',
             'app/*.controller.js',
 
             'app/*.spec.js'
@@ -16,6 +22,7 @@ module.exports = function (config) {
         browsers: ['PhantomJS'],
         plugins: [
             'karma-jasmine',
+            'karma-firefox-launcher',
             'karma-phantomjs-launcher',
             'karma-coverage'
         ],
