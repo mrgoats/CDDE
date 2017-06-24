f1174c5d75880010ab4ab01f0bef47361f236d06
Douglas Ferreira <drdocf@outlook.com>
2017-06-23 23:14:29 -0300

    Monet - US-13 Como um usuário, eu quero poder exportar o meu projeto para poder salvar o meu trabalho

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -6,18 +6,41 @@
         .module('cdde')
         .controller('mainController', mainController);
 
-    mainController.$inject = ['$log'];
+    mainController.$inject = ['$log', 'mainService'];
 
-    function mainController($log) {
+    function mainController($log, mainService) {
 
         var vm = this;
 
         vm.activate = activate;
+        vm.download = download;
+        vm.downloadJSON = downloadJSON;
 
         vm.activate();
 
         function activate() {
-            $log.info('OK');
+            $log.info('OK - Main App');
+
+        };
+
+        function download(text, name, type) {
+            var a = document.createElement("a");
+            var file = new Blob([text], {
+                type: type
+            });
+            a.href = URL.createObjectURL(file);
+            a.download = name;
+            a.click();
+        };
+
+        function downloadJSON() {
+
+            var shapes = mainService.getForms();
+
+            var jsonData = JSON.stringify(shapes);
+
+            vm.download(jsonData, 'project.json', 'application/json');
+
         };
 
     };
